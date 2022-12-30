7eb7206bb05b7b6f5726f8dd2e1da31872ad892c
moddas311 <moddasir311@gmail.com>
2022-12-30 23:46:40 +0600

    name changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import toast, { Toaster } from 'react-hot-toast';
-import { useContext } from 'react';
+import { Toaster } from 'react-hot-toast';
 import './App.css';
-import { AuthContext } from './AuthProvider/AuthProvider';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes/Routes';
 
 
 function App() {
-
-  const { user } = useContext(AuthContext);
-
-
   return (
     <div className='max-w-[1440px] mx-auto'>
       <RouterProvider
